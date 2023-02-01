37a45c6f227ae438ce047118a3737326c6b3368b
yash.j <yash.jaiswal@oncehub.com>
2023-02-01 16:12:34 +0530

    conditional syntax

---
+++ conditionals.rb
@@ -0,0 +1,34 @@
+# Case
+
+num = 'four'
+
+case num
+
+when "one"
+    puts 'num is 1'
+
+when "two"
+    puts 'num is 2'
+
+when "three"
+    puts 'num is 3'
+
+when "four"
+    puts 'num is 4'
+
+else
+    puts 'Default'
+
+end
+
+str = 'four'
+# If Else If
+if str == 'one'
+    puts 'num is 1'
+
+elsif str == 'four'
+    puts 'num is 4'
+
+else 
+    puts 'not in options.'
+end
