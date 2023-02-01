be8439a9f99cd2b4d9d3b42be8cdc4e38b22033b
yash.j <yash.jaiswal@oncehub.com>
2023-02-01 15:40:28 +0530

    methods syntax

---
+++ methods.rb
@@ -0,0 +1,21 @@
+# Syntax
+def test
+    puts 'Hello World!!'
+end
+
+test
+
+# With Parameters
+# Syntax 1
+def test msg
+    puts msg
+end
+
+test('Hi')
+
+# Syntax 2
+def test(msg)
+    puts msg
+end
+
+test('Hi!!')
