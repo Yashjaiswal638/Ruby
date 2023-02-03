e6c42dafece062517891553e460b2a49f87fe407
yash.j <yash.jaiswal@oncehub.com>
2023-02-03 18:24:39 +0530

    area code program skeleton

---
+++ area_code.rb
@@ -0,0 +1,17 @@
+dial_book = {
+    "newyork" => "212",
+    "newbrunswick" => "732",
+    "edison" => "908",
+    "plainsboro" => "609",
+    "sanfrancisco" => "301",
+    "miami" => "305",
+    "paloalto" => "650",
+    "evanston" => "847",
+    "orlando" => "407",
+    "lancaster" => "717"
+  }
+
+def get_city_names(city_list)
+    return city_list.keys
+end
+
