8a350babc3984849d4dd27ee1dac4eaf51915b02
yash.j <yjaiswal638@gmail.com>
2023-02-15 18:57:04 +0530

    Added all basic functionality for CRUD operations.

--- Rails_Projects/alpha-blog/app/controllers/articles_controller.rb
+++ Rails_Projects/alpha-blog/app/controllers/articles_controller.rb
@@ -6,7 +6,49 @@ class ArticlesController < ApplicationController
 
 
     def show
-        # @article = Article.find(params[:id])
+        @article = Article.find(params[:id])
+    end
+
+
+    def new
+        @article = Article.new
+    end
+
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+
+    def create
+        # render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        # render plain: @article.inspect
+        if @article.save
+        # redirect_to article_path(@article)
+            flash[:notice] = "Article was created successfully."
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+          flash[:notice] = "Article was updated successfully."
+          redirect_to @article
+        else
+          render 'edit', status: :unprocessable_entity
+        end
+    end
+
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
     end
 
 end
--- Rails_Projects/alpha-blog/config/routes.rb
+++ Rails_Projects/alpha-blog/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show, :index]
+  resources :articles#, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 
   # Defines the root path route ("/")
   # root "articles#index"
