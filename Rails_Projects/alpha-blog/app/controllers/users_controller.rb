413a1fcb55aed5a278dc48367c40d5ba0ee43d54
yash.j <yjaiswal638@gmail.com>
2023-02-18 23:49:58 +0530

    Completed user sign up & user profile edit.

--- Rails_Projects/alpha-blog/app/controllers/users_controller.rb
+++ Rails_Projects/alpha-blog/app/controllers/users_controller.rb
@@ -5,17 +5,41 @@ class UsersController < ApplicationController
     end
 
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+
     def create
         # render plain: params[:article]
-        @user = User.new(params.require(:user).permit(:username, :email, :password))
+        @user = User.new(user_params)
         # render plain: @article.inspect
         if @user.save
         # redirect_to article_path(@article)
-            flash[:notice] = "User Sign up successfull."
-            redirect_to @user
+            flash[:notice] = "User Sign up successfull, Welcome #{@user.username}."
+            redirect_to articles_path
         else
-            render 'new'
+            render 'new', status: :unprocessable_entity
         end
     end
+
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+          flash[:notice] = "User was updated successfully."
+          redirect_to articles_path
+        else
+          render 'edit', status: :unprocessable_entity
+        end
+    end
+
+    private
+
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 
 end
--- Rails_Projects/alpha-blog/config/routes.rb
+++ Rails_Projects/alpha-blog/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root "pages#home"
+
   resources :articles#, only: [:show, :index, :new, :create, :edit, :update, :destroy]
-  
+
   get 'signup' => 'users#new'
-  post 'users' => 'users#create'
-  # resources :users, except: [:new]
+  # post 'users' => 'users#create'
+  resources :users, except: [:new]
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
