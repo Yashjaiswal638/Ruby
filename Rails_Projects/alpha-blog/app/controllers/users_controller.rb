73b0964aa00605c7265f1ae444408b42e13a6267
yash.j <yjaiswal638@gmail.com>
2023-02-20 14:05:31 +0530

    Added pagination, users listing page, show users page

--- Rails_Projects/alpha-blog/app/controllers/users_controller.rb
+++ Rails_Projects/alpha-blog/app/controllers/users_controller.rb
@@ -5,6 +5,17 @@ class UsersController < ApplicationController
     end
 
 
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
+
+    
+    def index
+        @users = User.paginate(page: params[:page], per_page: 2)
+    end
+
+
     def edit
         @user = User.find(params[:id])
     end
@@ -28,7 +39,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
           flash[:notice] = "User was updated successfully."
-          redirect_to articles_path
+          redirect_to @user
         else
           render 'edit', status: :unprocessable_entity
         end
