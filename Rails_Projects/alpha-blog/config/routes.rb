c7c7f4ae753ff69f57a7882eaf774c96ae3baead
yash.j <yjaiswal638@gmail.com>
2023-02-20 15:40:09 +0530

    Added Navbar, completed create & destroy for user sessions

--- Rails_Projects/alpha-blog/config/routes.rb
+++ Rails_Projects/alpha-blog/config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   # post 'users' => 'users#create'
   resources :users, except: [:new]
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   # Defines the root path route ("/")
 end
