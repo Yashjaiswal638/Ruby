d999edfb3678abf052aabb6a20246705a89b6478
yashj <yjaiswal638@gmail.com>
2023-04-07 21:09:53 +0530

    added basic functionality

--- Rails_Projects/foodlog/app/models/entry.rb
+++ Rails_Projects/foodlog/app/models/entry.rb
@@ -1,2 +1,13 @@
 class Entry < ApplicationRecord
+    # validates ${:calories, :proteins, :carbs, :fats, :meal_type}, presence :true
+    validates :meal_type, presence: true
+    validates :calories, presence: true
+    validates :proteins, presence: true
+    validates :carbs, presence: true
+    validates :fats, presence: true
+
+    def day
+        self.created_at.strftime("%b %e, %Y")
+    end
+
 end
